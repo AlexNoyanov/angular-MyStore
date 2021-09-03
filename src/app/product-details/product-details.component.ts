b046c4eb13e0c0e512aa1bc9392d33661b00d2bf
AlexNoyanov <alex.noyanov@gmail.com>
2021-09-03 22:16:36 +0300

    2 files modified, 1 added, Shopping cart created - empty

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -19,12 +19,22 @@ export class ProductDetailsComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-  // First get the product id from the current route.
+    // First get the product id from the current route.
   const routeParams = this.route.snapshot.paramMap;
   const productIdFromRoute = Number(routeParams.get('productId'));
 
   // Find the product that correspond with the id provided in route.
   this.product = products.find(product => product.id === productIdFromRoute);
   }
+
+    
+    
+  // First get the product id from the current route.
+  // const routeParams = this.route.snapshot.paramMap;
+  // const productIdFromRoute = Number(routeParams.get('productId'));
+
+  // // Find the product that correspond with the id provided in route.
+  // this.product = products.find(product => product.id === productIdFromRoute);
+  // }
 
 }
